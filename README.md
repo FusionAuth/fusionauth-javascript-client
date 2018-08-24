34263ff86c2e79cf3a3840a58e8ab52d0322679d
Brian Pontarelli <brian@inversoft.com>
2018-08-24 13:31:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## FusionAuth Javascript Client ![semver 2.0.0 compliant](http://img.shields.io/badge/semver-2.0.0-brightgreen.svg?style=flat-square)
+If you're integrating FusionAuth with a Javascript application, this library will speed up your development time.
+
+For additional information and documentation on FusionAuth refer to [https://fusionauth.io](https://fusionauth.io).
+
+Refer to the FusionAuth API documentation to for request and response formats. 
+* https://fusionauth.io/docs/v1/tech/apis/
+* https://fusionauth.io/docs/v1/tech/client-libraries/javascript
---
+++ tests/js/FusionAuthClientTest.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2018, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+'use strict';
+
+var assert = buster.assertions.assert;
+
+buster.testCase('FusionAuthClient', {
+
+  setUp: function() {
+    this.client = new FusionAuthClient('bf69486b-4733-4470-a592-f1bfce7af580', 'http://localhost:9011');
+  },
+
+  'retrieveApplicationsTest': function(done) {
+    this.client.retrieveApplications(done(function(response) {
+      assert.equals(response.statusCode, 200);
+      assert.defined(response.successResponse);
+
+      if (response.successResponse) {
+        assert.isTrue(response.successResponse.applications.length > 0);
+      }
+    }));
+  },
+
+  'retrieveFusionAuthApplicationTest': function(done) {
+    this.client.retrieveApplication('3c219e58-ed0e-4b18-ad48-f4f92793ae32', done(function(response) {
+      assert.equals(response.statusCode, 200);
+      assert.defined(response.successResponse);
+
+      if (response.successResponse) {
+        assert.equals(response.successResponse.application.name, 'FusionAuth');
+      }
+    }));
+  },
+
+  'retrieveAdminUserTest': function(done) {
+    this.client.retrieveUserByEmail('admin@fusionauth.io', done(function(response) {
+      assert.equals(response.statusCode, 200);
+      assert.defined(response.successResponse);
+
+      if (response.successResponse) {
+        assert.equals(response.successResponse.user.email, 'admin@fusionauth.io');
+      }
+    }));
+  },
+
+  'retrieveSystemConfigurationTest': function(done) {
+    this.client.retrieveSystemConfiguration(done(function(response) {
+      assert.equals(response.statusCode, 200);
+      assert.defined(response.successResponse);
+
+      if (response.successResponse) {
+        assert.isTrue(response.successResponse.systemConfiguration.minimumPasswordAge.seconds > 0);
+      }
+    }));
+  },
+
+  'createUserTest': function(done) {
+    var uuid = function() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }();
+
+    this.client.createUser(uuid, {
+      'user': {
+        'email': uuid + '@fusionauth.io',
+        'password': 'foobarbaz'
+      }
+    }, done(function(response) {
+      assert.equals(response.statusCode, 200);
+      assert.defined(response.successResponse);
+
+      if (response.successResponse) {
+        assert.isTrue(response.successResponse.user.email === uuid + '@fusionauth.io');
+      }
+    }));
+
+    this.client.deleteUser(uuid, function(response) {
+
+    });
+  }
+});
